4b7bde4aebfc375c276a102586db4c85ffb7f48a
Shivani Yadav <125184341+Shivani3060@users.noreply.github.com>
2023-03-17 09:14:11 -0700

    17/03/2023

---
+++ fab.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.naming.spi.ResolveResult;
+
+public class fab {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("number of elements");
+        int size = sc.nextInt();
+        System.out.println("enter the num one");
+        int num1 = sc.nextInt();
+System.out.println("enter the num 2");
+int num2 =sc.nextInt();
+//logic
+for(int i=0;i<size;i++){
+
+int res= num1 +num2;
+System.out.println(res);
+    num1=num2;
+    num2=res;
+}
+    
+}
+}
