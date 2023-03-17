9e84d494ef071813603319f549f4d0d46f4bcee6
Shivani Yadav <125184341+Shivani3060@users.noreply.github.com>
2023-03-17 05:52:44 -0700

    17/02/2023

---
+++ neonNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class neonNum {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the num");
+        int num= sc.nextInt();
+        //logic
+        int res, rem,mod;
+        res= num*num;
+        rem = num%10;
+        mod=res+rem;
+
+
+        
+
+    
+    }
+   
+    
+}
